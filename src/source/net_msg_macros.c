7cb2dd28982d9c872b5d8eb3da8ae05f9d633a70
Yuri Andriaccio <yurand2000@gmail.com>
2021-06-25 10:58:30 +0200

    Commit 20n

--- src/source/net_msg_macros.c
+++ src/source/net_msg_macros.c
@@ -6,7 +6,7 @@ int is_server_message(net_msg* message, msg_t message_num)
 	if (ret == -1) return -1;
 	if (!ret &&
 		ISSERVER(message->type) &&
-		(GETSERVER(message->type) == (message_num & MESSAGE_SERVER_MASK)))
+		CHECK_SERVER_MSG_TYPE(message->type, message_num & MESSAGE_SERVER_MASK))
 		return 0;
 	else
 		return 1;
@@ -18,7 +18,7 @@ int is_client_message(net_msg* message, msg_t message_num)
 	if (ret == -1) return -1;
 	if (!ret &&
 		ISCLIENT(message->type) &&
-		(GETCLIENT(message->type) == (message_num & MESSAGE_CLIENT_MASK)))
+		CHECK_CLIENT_MSG_TYPE(message->type, message_num & MESSAGE_CLIENT_MASK))
 		return 0;
 	else
 		return 1;
