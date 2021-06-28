13ee3f260858e04f20cf530edf1d54a6dda16dc1
Yuri Andriaccio <yurand2000@gmail.com>
2021-06-28 12:19:26 +0200

    Commit 29

--- src/exec/dummy_gen.sh
+++ src/exec/dummy_gen.sh
@@ -1,12 +1,13 @@
-#!bin/bash
+#!/bin/bash
 
 #generates $1 random files with random text, of size between $2 and $3, in folder $4
 chars="abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ.,?/+=-*/;:[]{}()@"
 len=$3-$2
-for f in {1..$1} ; do
-  size = ($RANDOM % $len) + $2
+elems={1..$1}
+for ((f = 0; f < $1; f++)); do
+  size=($RANDOM%$len)+$2+1
   echo -n > "$4/file_$f.txt"
-  for i in {$2..$size} ; do
+  for ((i=$2; i < $size; i++)); do
     echo -n "${chars:($RANDOM)%${#chars}:1}" >> "$4/file_$f.txt"
   done
 done
